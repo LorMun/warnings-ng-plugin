40904954705a4a833ad774e278ffe3986bfb8da7
LorMun <Lorenz.Munsch@gmx.de>
2019-06-13 15:25:05 +0200

    container test

--- src/test/java/io/jenkins/plugins/analysis/warnings/DockerITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/DockerITest.java
@@ -1,7 +1,9 @@
-package io.jenkins.plugins.analysis.warnings.recorder;
+package io.jenkins.plugins.analysis.warnings;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.apache.commons.io.output.TeeOutputStream;
@@ -18,10 +20,10 @@ import hudson.Launcher;
 import hudson.model.Descriptor.FormException;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
-import hudson.model.Slave;
 import hudson.plugins.sshslaves.SSHLauncher;
-import hudson.security.ACL;
 import hudson.slaves.DumbSlave;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import hudson.slaves.EnvironmentVariablesNodeProperty.Entry;
 import hudson.tasks.Maven;
 import hudson.tasks.Shell;
 import hudson.util.StreamTaskListener;
@@ -31,11 +33,10 @@ import jenkins.security.s2m.AdminWhitelistRule;
 
 import io.jenkins.plugins.analysis.core.model.AnalysisResult;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerTest;
-import io.jenkins.plugins.analysis.warnings.Cmake;
 import io.jenkins.plugins.analysis.warnings.Gcc3;
+import io.jenkins.plugins.analysis.warnings.GccContainer;
 import io.jenkins.plugins.analysis.warnings.MavenConsole;
 
-import static io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerSuite.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assume.*;
@@ -43,6 +44,10 @@ import static org.junit.Assume.*;
 public class DockerITest extends IntegrationTestWithJenkinsPerTest {
     @Rule
     public DockerRule<JavaContainer> javaDockerRule = new DockerRule<>(JavaContainer.class);
+
+    @Rule
+    public DockerRule<GccContainer> gccDockerRule = new DockerRule<>(GccContainer.class);
+
 
     @BeforeClass
     public static void assumeThatWeAreRunningLinux() throws Exception {
@@ -59,6 +64,10 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
                 new VersionNumber(baos.toString().trim()), greaterThanOrEqualTo(new VersionNumber("1.13.0")));
     }
 
+    /**
+     * Integrationstest Aufgabe 2. Building with Java Files
+     * @throws Exception
+     */
     @Test
     public void shouldBuildJavaFileOnDumbSlave() throws Exception {
         DumbSlave slave = jenkinsPerTest.createOnlineSlave();
@@ -81,6 +90,10 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
         assertThat(result).isNotNull();
     }
 
+    /**
+     * Integrationstest Aufgabe 2. Build with makefile
+     * @throws Exception
+     */
     @Test
     public void shouldBuildCFileOnDumbSlave() throws Exception {
         DumbSlave worker = jenkinsPerTest.createOnlineSlave();
@@ -104,6 +117,10 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
         assertThat(result).isNotNull();
     }
 
+    /**
+     * Integrationstest Aufgabe 2. Building with Security enabled
+     * @throws Exception
+     */
     @Test
     public void shouldBuildJavaFileOnDumbSlaveWithSecurityEnabled() throws Exception {
         DumbSlave slave = createDumbSlaveWithEnabledSecurity();
@@ -125,31 +142,113 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
         assertThat(result).isNotNull();
     }
 
+    /**
+     * Class for building DumbSlave with Enabled Security
+     * @return DumbSlave with enabled Security
+     * @throws Exception
+     */
     public DumbSlave createDumbSlaveWithEnabledSecurity() throws Exception {
-            DumbSlave agent = jenkinsPerTest.createOnlineSlave();
+        DumbSlave agent = jenkinsPerTest.createOnlineSlave();
 
-            FilePath child = getJenkins().getInstance().getRootPath().child("secrets/filepath-filters.d/30-default.conf");
-            child.delete();
-            child.write("", "ISO_8859_1");
+        FilePath child = getJenkins().getInstance().getRootPath().child("secrets/filepath-filters.d/30-default.conf");
+        child.delete();
+        child.write("", "ISO_8859_1");
 
-            Objects.requireNonNull(getJenkins().jenkins.getInjector())
-                    .getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false);
-            getJenkins().jenkins.save();
-            return agent;
+        Objects.requireNonNull(getJenkins().jenkins.getInjector())
+                .getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false);
+        getJenkins().jenkins.save();
+        return agent;
     }
 
     @Test
     public void shouldStartAgent() {
-        DumbSlave agent = createAgent();
+        DumbSlave agent = createJavaAgent();
 
         assertThat(agent.getWorkspaceRoot().getName()).isEqualTo("workspace");
     }
 
-    private DumbSlave createAgent() {
+    /*
+     * Integrationstest Aufgabe 3., Building With Java Container
+     */
+    @Test
+    public void shouldBuildJavaProjectWithContainer() {
+        DumbSlave agent = createJavaAgent();
+
+        FreeStyleProject project = createFreeStyleProject();
+        try {
+            project.setAssignedNode(agent);
+        }
+        catch (IOException e) {
+            throw new AssertionError(e);
+        }
+
+        project.getBuildersList().add(new Maven("verify", null));
+        copySingleFileToAgentWorkspace(agent, project, "JavaHelloWorldForDockerITest.java",
+                "src/main/java/HelloWorld.java");
+        copySingleFileToAgentWorkspace(agent, project, "PomForDockerITest.xml", "pom.xml");
+        enableWarnings(project, createTool(new MavenConsole(), ""));
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertThat(result).isNotNull();
+    }
+
+    private DumbSlave createJavaAgent() {
         try {
             JavaContainer javaContainer = javaDockerRule.get();
 
             DumbSlave agent = createAgent(javaContainer);
+
+            List<EnvironmentVariablesNodeProperty>  properties = new ArrayList<>();
+            properties.add(new EnvironmentVariablesNodeProperty(new Entry("JAVA_HOME", "/usr/lib/jvm/java-8-openjdk-amd64/jre")));
+            agent.setNodeProperties(properties);
+            getJenkins().jenkins.addNode(agent);
+            getJenkins().waitOnline(agent);
+
+            return agent;
+        }
+        catch (Exception e) {
+            throw new AssertionError(e);
+        }
+
+    }
+
+    /**
+     * Integrationstest Aufgabe 3. Build with makefile and GccContainer
+     * @throws Exception
+     */
+    @Test
+    public void shouldBuildGccFileWithContainer() throws Exception {
+        DumbSlave worker = createGccAgent();
+        FreeStyleProject project = createFreeStyleProject();
+        try {
+            project.setAssignedNode(worker);
+        }
+        catch (IOException exception) {
+            throw new AssertionError(exception);
+        }
+        buildSuccessfully(project);
+
+        project.getBuildersList().add(new Shell("make"));
+        copySingleFileToAgentWorkspace(worker, project, "CHelloWorldMakeFileForDockerITest", "makefile");
+        copySingleFileToAgentWorkspace(worker, project, "CHelloWorldForDockerITest.c", "testClass.c");
+        enableWarnings(project, createTool(new Gcc3(), ""));
+
+        scheduleSuccessfulBuild(project);
+
+        AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        assertThat(result).isNotNull();
+    }
+
+
+    private DumbSlave createGccAgent() {
+        try {
+            GccContainer gccContainer = gccDockerRule.get();
+
+            DumbSlave agent = createAgent(gccContainer);
+
+//            List<EnvironmentVariablesNodeProperty>  properties = new ArrayList<>();
+//            properties.add(new EnvironmentVariablesNodeProperty(new Entry("JAVA_HOME", "/usr/lib/jvm/java-8-openjdk-amd64/jre")));
+//            agent.setNodeProperties(properties);
             getJenkins().jenkins.addNode(agent);
             getJenkins().waitOnline(agent);
 
